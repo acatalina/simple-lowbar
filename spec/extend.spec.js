c15b8397594ca39c4f2a7ca93c5a6f3138966475
acatalina <accarrasco@gmail.com>
2017-03-22 18:30:54 +0000

    create single file test for extend

---
+++ spec/extend.spec.js
@@ -0,0 +1,40 @@
+/* global describe, it */
+const path = require('path');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const _ = require(path.join(__dirname, '..', './lowbar.js'));
+
+describe('#extend', function () {    
+  it('is a function',function(){
+    expect(_.extend).to.be.a('function')
+  });
+
+  it('returns the destination when no sources are provided', function(){
+    expect(_.extend({'a': 1, 'b': 2})).to.eql({'a': 1, 'b': 2});   
+  });
+
+  it('copies the properties of the source object into the target object', function () {
+    let actual = _.extend({}, {name: 'Sam', age: 20});
+    let expected = {name: 'Sam', age: 20};
+    expect(actual).to.eql(expected);
+  });
+  
+  it('overwrites existing properties', function () {
+    let actual = _.extend({name: 'Joe'}, {name: 'Sam', age: 20});
+    let expected = {name: 'Sam', age: 20};
+    expect(actual).to.eql(expected);
+  });
+  
+  it('copies properties from multiple source arguments', function () {
+    let actual = _.extend({name: 'Sam'}, {name: 'Joe'}, {name: 'Pepe', age: 99});
+    let expected = {name: 'Pepe', age: 99};
+    expect(actual).to.eql(expected);
+  });
+  
+  it('works for arrays', function () {
+    let actual = _.extend([1, 2, 3], [4, 5, 6], {name: 'Pepe'});
+    let expected = [4, 5, 6];
+    expected.name = 'Pepe';
+    expect(actual).to.eql(expected);
+  });
+});
