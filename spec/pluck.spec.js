91df6dd3c50066d76b43ba607fe2ba74709f5410
acatalina <accarrasco@gmail.com>
2017-03-22 12:06:25 +0000

    extra test for invalid inputs

--- spec/pluck.spec.js
+++ spec/pluck.spec.js
@@ -1,16 +1,32 @@
 /* global describe, it */
 const path = require('path');
 const expect = require('chai').expect;
-const sinon = require('sinon');
 const _ = require(path.join(__dirname, '..', './lowbar.js'));
 
-describe('_.pluck', function () {
+describe('_.pluck', function() {
   it('is a function', function() {
     expect(_.pluck).to.be.a('function');
   });
 
-  it('extracts property values', function () {
-    var pluck = _.pluck([{num: 1, c: 0 }, {num: 2, c: 3}, {num:3, c:1}], 'num');
-    expect(pluck).to.eql([1, 2, 3]);
+  it('manages invalid inputs returning an empty array', function() {
+    expect(_.pluck()).to.eql([]);
+    expect(_.pluck(1)).to.eql([]);
+    expect(_.pluck(NaN)).to.eql([]);
+  });
+
+  it('extracts property values', function() {
+    let actual = _.pluck([
+      {num: 1, c: 0}, 
+      {num: 2, c: 3}, 
+      {num: 3, c: 1}
+      ], 'num'); 
+    let expected = [1, 2, 3];
+    expect(actual).to.eql(expected);
+  });
+
+  it('returns an undefined array when no properties are found', function() {
+    let actual = _.pluck([1])
+    let expected = [undefined];
+    expect(actual).to.eql(expected);
   });
 });
