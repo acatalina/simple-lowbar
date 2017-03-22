34e8d75eeab0be7f9df109016aa1eebd12c9166d
acatalina <accarrasco@gmail.com>
2017-03-22 17:58:53 +0000

    test for invalid inputs and binding to context

--- spec/every.spec.js
+++ spec/every.spec.js
@@ -4,24 +4,36 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const _ = require(path.join(__dirname, '..', './lowbar.js'));
   
-describe('#every', function () {    
+describe('_.every', function () {    
   it('is a function', function() {
     expect(_.every).to.be.a('function');
   });
 
-  it('returns false and stops counting if one value does not pass the criteria', function () {
-    var spy = sinon.spy(function(n) { return n < 3; })
-    var expected = false;
-    var actual = _.every([1, 2, 3, 4, 5], spy);
+  it('returns true for invalid inputs', function() {
+    expect(_.every()).to.eql(true);
+    expect(_.every(1)).to.eql(true);
+    expect(_.every(NaN)).to.eql(true);
+  });
+
+  it('returns false and stops checking if one value does not pass the criteria', function () {
+    let spy = sinon.spy(function(n) { return n < 3; })
+    let expected = false;
+    let actual = _.every([1, 2, 3, 4, 5], spy);
     expect(actual).to.eql(expected);
     expect(spy.callCount).to.eql(3);
   });
 
-  it('returns true when all items are true', function () {
-    var spy = sinon.spy(function(n) { return n < 6; })
-    var expected = true;
-    var actual = _.every([1, 2, 3, 4, 5], spy);
+  it('returns true when all items pass the criteria', function () {
+    let spy = sinon.spy(function(n) { return n < 6; })
+    let expected = true;
+    let actual = _.every([1, 2, 3, 4, 5], spy);
     expect(actual).to.eql(expected);
     expect(spy.callCount).to.eql(5);
+  });
+
+  it('binds the iteratee to the context given', function() {
+    let spy = sinon.spy();
+    _.every([1, 2, 3], spy, [2, 3, 4]);
+    expect(spy.thisValues[0]).to.eql([2, 3, 4]);
   });
 });
